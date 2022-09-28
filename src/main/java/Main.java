dcea2555f23546a9f6d8a521928919a08b12d578
Marek Fijalkowski <gitmaro1983@gmail.com>
2022-09-28 21:09:41 +0200

    first commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        try {
+            int value = 10 / 0;
+        } catch (ArithmeticException exception) {
+            exception.printStackTrace();
+        } finally {
+            System.out.println("Finally");
+        }
+
+
+        try {
+            throw new TestException();
+        } catch (TestException exception) {
+            exception.printStackTrace();
+        }
+    }
+}
